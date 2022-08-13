a527e13c1ef621c24d18125bc8d98941059f0e8a
imkarthi16 <karthiveera@gmail.com>
2022-08-13 13:48:10 +0530

    Working function props

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,20 +1,26 @@
-import React, { useRef } from "react";
+import { useRef } from 'react';
 
-const NewTodo = () => {
+const NewTodo: React.FC<{onAddTodo: (text: string) => void }> = (props) => {
   const todoTextInputRef = useRef<HTMLInputElement>(null);
+
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
+
     const enteredText = todoTextInputRef.current!.value;
+
     if (enteredText.trim().length === 0) {
       // throw an error
       return;
     }
+
+    props.onAddTodo(enteredText);
   };
+
   return (
     <form onSubmit={submitHandler}>
-      <label htmlFor="text">ToDo Text</label>
-      <input type="text" id="text" ref={todoTextInputRef} />
-      <button>Add ToDo</button>
+      <label htmlFor='text'>Todo text</label>
+      <input type='text' id='text' ref={todoTextInputRef} />
+      <button>Add Todo</button>
     </form>
   );
 };
