6015d0af3c84c8744a46f5da884c815c2ad238ba
imkarthi16 <karthiveera@gmail.com>
2022-08-13 11:48:29 +0530

    Working with props & Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import Todos from './components/Todos';
+
 function App() {
   return (
     <div>
-      <Todos/>
+      <Todos items={['Learn React', 'Learn TypeScript']} />
     </div>
   );
 }
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,9 +1,13 @@
-function Todos(){
-    return(
-       <ul>
-        <li>Learn React</li>
-        <li>Learn TypeScript</li>
-       </ul>
-    )
-}
+import React from 'react';
+
+const Todos: React.FC<{ items: string[] }> = (props) => {
+  return (
+    <ul>
+      {props.items.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
+    </ul>
+  );
+};
+
 export default Todos;
